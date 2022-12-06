62dc4191e5cf2af51757dfe4f6e29ab09490b436
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-12-06 20:53:34 +0100

    seeding test data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,4 +10,9 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+alias PhAuthApi.Repo
+alias PhAuthApi.EligibleData.Office
 
+Repo.insert! %Office{name: "test1", address: "test1", email: "test1@mail.com"}
+Repo.insert! %Office{name: "test2", address: "test2", email: "test2@mail.com"}
+Repo.insert! %Office{name: "test3", address: "test3", email: "test3@mail.com"}
